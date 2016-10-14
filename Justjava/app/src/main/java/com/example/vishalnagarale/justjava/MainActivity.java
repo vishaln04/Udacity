e391dec6a5a122bcbd56c65253402dbc6627854c
VishalNagarale\Vishal Nagarale <vishalnagarale@gmail.com>
2016-10-14 16:08:14 +0530

    Include price textviews

--- Justjava/app/src/main/java/com/example/vishalnagarale/justjava/MainActivity.java
+++ Justjava/app/src/main/java/com/example/vishalnagarale/justjava/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.vishalnagarale.justjava;
 
+import android.icu.text.NumberFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,11 +15,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view){
-        display(1);
+        display(2);
+        displayPrice(2*5);
     }
 
+    // Display Number of Cups of coffee
     private void display(int number){
         TextView quantityTextView = (TextView)findViewById(R.id.value);
         quantityTextView.setText("" + number);
+    }
+
+    // Display total price of coffee
+    private void displayPrice(int number){
+        TextView priceTextView = (TextView)findViewById(R.id.pvalue);
+        priceTextView.setText("$" + number);
     }
 }
