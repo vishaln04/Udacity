326ad25b477b008eb8051b595a19eaac1441cbb9
VishalNagarale\Vishal Nagarale <vishalnagarale@gmail.com>
2016-10-14 17:05:46 +0530

    Project Completed

--- Justjava/app/src/main/java/com/example/vishalnagarale/justjava/MainActivity.java
+++ Justjava/app/src/main/java/com/example/vishalnagarale/justjava/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.vishalnagarale.justjava;
 
+import android.content.Context;
 import android.icu.text.NumberFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public int Quantity = 0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,20 +21,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void submitOrder(View view){
-        display(2);
-        displayPrice(2*5);
+    public void submitOrder(View view) {
+        display(Quantity);
+        displayPrice(Quantity * 5);
     }
 
     // Display Number of Cups of coffee
-    private void display(int number){
-        TextView quantityTextView = (TextView)findViewById(R.id.value);
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.value);
         quantityTextView.setText("" + number);
     }
 
     // Display total price of coffee
-    private void displayPrice(int number){
-        TextView priceTextView = (TextView)findViewById(R.id.pvalue);
-        priceTextView.setText("$" + number);
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.pvalue);
+        priceTextView.setText("" + number);
+    }
+
+    // increment function
+    public void increment(View view) {
+        Quantity += 1;
+        TextView quantityTextView = (TextView) findViewById(R.id.value);
+        quantityTextView.setText("" + Quantity);
+    }
+
+    // decrement function
+    public void decrement(View view) {
+        Context context = getApplicationContext();
+        TextView quantityTextView = (TextView) findViewById(R.id.value);
+        if (Quantity == 0)
+            Toast.makeText(context, "Cannot decremented", Toast.LENGTH_SHORT).show();
+        else
+            Quantity -= 1;
+        quantityTextView.setText("" + Quantity);
     }
 }
