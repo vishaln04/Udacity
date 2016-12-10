a01c07411732a171cfa4be5ac5da23d781c7b783
vishaln04 <vishalnagarale@gmail.com>
2016-12-10 10:29:42 +0530

    Adding styles

--- Justjava/app/src/main/java/com/example/vishalnagarale/justjava/MainActivity.java
+++ Justjava/app/src/main/java/com/example/vishalnagarale/justjava/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.vishalnagarale.justjava;
 
 import android.content.Context;
 import android.content.Intent;
-import android.icu.text.NumberFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -29,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
        CheckBox checkBox_chocolate = (CheckBox) findViewById(R.id.checkbox_chocolate);
         EditText name = (EditText) findViewById(R.id.user_name);
         String u_name = name.getText().toString();
-        u_name = "JustJava order for " + u_name;
+        u_name = getResources().getString(R.string.just_order) + u_name;
         String message = createOrderSummary(Quantity,u_name,checkBox_whipped_cream.isChecked(), checkBox_chocolate.isChecked());
 
         Intent i = new Intent(Intent.ACTION_SEND);
@@ -38,9 +37,9 @@ public class MainActivity extends AppCompatActivity {
         i.putExtra(Intent.EXTRA_SUBJECT, u_name);
         i.putExtra(Intent.EXTRA_TEXT   , message);
         try {
-            startActivity(Intent.createChooser(i, "Send mail..."));
+            startActivity(Intent.createChooser(i, getResources().getString(R.string.sendmail)));
         } catch (android.content.ActivityNotFoundException ex) {
-            Toast.makeText(MainActivity.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, getResources().getString(R.string.noclients), Toast.LENGTH_SHORT).show();
         }
 
 //
@@ -62,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
         Context context = getApplicationContext();
         TextView quantityTextView = (TextView) findViewById(R.id.value);
         if (Quantity == 0)
-            Toast.makeText(context, "Cannot decremented", Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, getResources().getString(R.string.toast), Toast.LENGTH_SHORT).show();
         else
             Quantity -= 1;
         quantityTextView.setText("" + Quantity);
@@ -71,14 +70,14 @@ public class MainActivity extends AppCompatActivity {
     // Creates an order summary
     private String createOrderSummary(int number, String name, boolean checkbox, boolean checkbox_choco){
         String message;
-        if (checkbox == false && checkbox_choco == false)
-            message = "Name : " + name + "\nQuantity : "+ number + "\nTotal : " + number*5 + "\nThank You!";
-        else if (checkbox == true && checkbox_choco == false)
-            message = "Name : " + name + "\nwith Whipped Cream \nQuantity : "+ number + "\nTotal : " + number*6 + "\nThank You!";
-        else if (checkbox == false && checkbox_choco == true)
-            message = "Name : " + name + "\nwith Chocolate \nQuantity : "+ number + "\nTotal : " + number*7 + "\nThank You!";
+        if (!checkbox && !checkbox_choco)
+            message = getResources().getString(R.string.username) + " : " + name + "\n" + getResources().getString(R.string.quantity) + " : "+ number + "\n"+ getResources().getString(R.string.total) + " : " + number*5 + "\n" + getResources().getString(R.string.tq);
+        else if (checkbox && !checkbox_choco)
+            message = getResources().getString(R.string.username) + " : " + name + "\n" + getResources().getString(R.string.with) + getResources().getString(R.string.whi_cream) + "\n" + getResources().getString(R.string.quantity) + " : "+ number + "\n"+ getResources().getString(R.string.total) + " : " + number*6 + "\n" + getResources().getString(R.string.tq);
+        else if (!checkbox && checkbox_choco)
+            message = getResources().getString(R.string.username) + " : " + name + "\n" + getResources().getString(R.string.with) + getResources().getString(R.string.chocolate) + "\n" + getResources().getString(R.string.quantity) + " : "+ number + "\n"+ getResources().getString(R.string.total) + " : " + number*7 + "\n" + getResources().getString(R.string.tq);
         else
-            message = "Name : " + name + "\nwith Whipped Cream and Chocolate \nQuantity : "+ number + "\nTotal : " + number*8 + "\nThank You!";
+            message = getResources().getString(R.string.username) + " : " + name + "\n" + getResources().getString(R.string.with) + getResources().getString(R.string.whi_cream) + "&" + getResources().getString(R.string.chocolate) + "\n" + getResources().getString(R.string.quantity) + " : "+ number + "\n"+ getResources().getString(R.string.total) + " : " + number*8 + "\n" + getResources().getString(R.string.tq);
         return message;
     }
 
