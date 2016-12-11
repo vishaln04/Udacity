cde273ab5997eddec566c7cfc6e20d59e08bc8ff
vishaln04 <vishalnagarale@gmail.com>
2016-12-11 16:59:18 +0530

    English numbers added using Arraylist

--- LangConverter/app/src/main/java/com/example/vishal/langconverter/NumbersActivity.java
+++ LangConverter/app/src/main/java/com/example/vishal/langconverter/NumbersActivity.java
@@ -2,6 +2,11 @@ package com.example.vishal.langconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words = new ArrayList<>();
+        words.add(0,"One");
+        words.add(1,"Two");
+        words.add(2,"Three");
+        words.add(3,"Four");
+        words.add(4,"Five");
+        words.add(5,"Six");
+        words.add(6,"Seven");
+        words.add(7,"Eight");
+        words.add(8,"Nine");
+        words.add(9,"Ten");
+
+        LinearLayout numbers = (LinearLayout) findViewById(R.id.activity_numbers);
+
+
+        for (int index = 0; index < words.size(); index++) {
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            numbers.addView(wordView);
+        }
     }
 }
