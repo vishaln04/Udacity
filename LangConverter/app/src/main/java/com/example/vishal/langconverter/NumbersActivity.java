cb059cd60f4e7adecfd9f4318d29939620f39f2d
vishaln04 <vishalnagarale@gmail.com>
2016-12-12 11:13:36 +0530

    Return back to ListView

--- LangConverter/app/src/main/java/com/example/vishal/langconverter/NumbersActivity.java
+++ LangConverter/app/src/main/java/com/example/vishal/langconverter/NumbersActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -32,7 +31,7 @@ public class NumbersActivity extends AppCompatActivity {
 
        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
 
-        GridView listView = (GridView) findViewById(R.id.list);
+        ListView listView = (ListView) findViewById(R.id.list);
 
         listView.setAdapter(itemsAdapter);
 
